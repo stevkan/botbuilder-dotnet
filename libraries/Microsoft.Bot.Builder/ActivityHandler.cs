acb7aadacb0dc4f1ab111eacfb254758ae48acda
virtual-josh <virtual-josh@server.no>
2020-05-01 14:43:18 -0700

    breaking the build

--- libraries/Microsoft.Bot.Builder/ActivityHandler.cs
+++ libraries/Microsoft.Bot.Builder/ActivityHandler.cs
@@ -67,6 +67,7 @@ namespace Microsoft.Bot.Builder
             switch (turnContext.Activity.Type)
             {
                 case ActivityTypes.Message:
+                    turnContext.Activity.Text = "this is will break the build";
                     await OnMessageActivityAsync(new DelegatingTurnContext<IMessageActivity>(turnContext), cancellationToken).ConfigureAwait(false);
                     break;
 
